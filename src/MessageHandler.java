c4bce8c230e19879f0d6ea826f5fc4ce769cadfd
andybhattacharyya <bhattacharyya.anand@gmail.com>
2024-07-05 11:42:50 -0400

    Initial regex expression to validating the structure of an HTTP GET Request.

--- src/MessageHandler.java
+++ src/MessageHandler.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.regex.Pattern;
 
 public class MessageHandler implements Runnable {
 
@@ -16,6 +17,16 @@ public class MessageHandler implements Runnable {
        this.HttpClientOutput = new PrintWriter(connection.getOutputStream(),true);
     }
 
+    private boolean validRequest(String req){
+        /**
+         * Request-Line regex checklist: 3 letter capital word =^GET , space=\s, /, space=\s, HTTP/any digit,.,any digit:
+         * Request-Line + Headers Checklist:
+         *
+         */
+        String regex_requestline ="^GET\\s/\\sHTTP/(1.0|1.1|2|3)(\\s|\\s\\s)";
+        String regex_body="(.+:\\s.+(\\s\\s|\\s))+\\s\\s";
+        return Pattern.matches(regex_requestline+regex_body,req);
+    }
     @Override
     public void run() {
         //Handling HTTP GET Method: (1) Validate REQ Line, (2) Validate Headers till CRLF
@@ -23,9 +34,15 @@ public class MessageHandler implements Runnable {
         try {
             char[] tmp = new char[HttpClient.getInputStream().available()];
             HttpClientInput.read(tmp);
-            //Obtained our string to apply regex filters
+            //Obtained our string to apply regex filters string is multilined denoted by ASCII control characters 13,10
             String req = new String(tmp);
             System.out.println(req);
+            System.out.println(validRequest(req));
+            /*
+            for(int i : req.getBytes()){
+                System.out.print(i+",");
+            }
+            */
         } catch(IOException e){}
     }
 }
